74d7e1daf6d8f5ec4a0a237278e4e74656c87f70
François Levasseur <francois_levasseur@hotmail.com>
2021-04-23 14:14:28 -0400

    feat: bring back lang-server, yargs and cli args

--- packages/nlu/src/index.ts
+++ packages/nlu/src/index.ts
@@ -1,27 +1,10 @@
 global['NativePromise'] = global.Promise
 
-import chalk from 'chalk'
 import path from 'path'
+import { getAppDataPath } from './app-data'
 import getos from './utils/getos'
 
 process.core_env = process.env as BotpressEnvironmentVariables
-
-export function getAppDataPath() {
-  const homeDir = process.env.HOME || process.env.APPDATA
-  if (homeDir) {
-    if (process.platform === 'darwin') {
-      return path.join(homeDir, 'Library', 'Application Support', 'botpress')
-    }
-
-    return path.join(homeDir, 'botpress')
-  }
-
-  console.error(
-    chalk.red(`Could not determine your HOME directory.
-Please set the environment variable "APP_DATA_PATH", then start Botpress`)
-  )
-  process.exit()
-}
 
 if (process.env.APP_DATA_PATH) {
   process.APP_DATA_PATH = process.env.APP_DATA_PATH
