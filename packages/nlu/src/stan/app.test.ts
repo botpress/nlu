589acc304aa1851557c8af8afa423522c7cd30e8
Simon-Pierre Gingras <892367+spg@users.noreply.github.com>
2021-06-01 09:48:43 -0400

    Can run tests

--- packages/nlu/src/stan/app.test.ts
+++ packages/nlu/src/stan/app.test.ts
@@ -2,11 +2,13 @@ import { createApp } from './app'
 import { makeEngine } from './engine'
 import Logger from '../utils/logger'
 import { StanOptions } from './config'
+import request from 'supertest'
+import path from 'path'
 
 const options: StanOptions = {
   host: 'localhost',
   port: 3200,
-  limitWindow: '',
+  limitWindow: '1m',
   limit: 1,
   bodySize: '',
   batchSize: 1,
@@ -21,20 +23,28 @@ const options: StanOptions = {
   ],
   ducklingURL: 'https://duckling.botpress.io',
   ducklingEnabled: true,
-  legacyElection: false
+  legacyElection: false,
+  modelDir: 'hello'
 }
 
 let app
 
-beforeAll(async () => {
-  const launcherLogger = Logger.sub('Launcher')
-  const engine = await makeEngine(options, launcherLogger)
-  app = createApp(options, engine, '1.0')
-})
+// beforeAll(async () => {
+//   const launcherLogger = Logger.sub('Launcher')
+//   console.log('PROJECT_LOCATION:')
+//   process.PROJECT_LOCATION = process.pkg
+//     ? path.dirname(process.execPath) // We point at the binary path
+//     : __dirname // e.g. /dist/..
+//   console.log(process.PROJECT_LOCATION)
+//   const engine = await makeEngine(options, launcherLogger)
+//   app = await createApp(options, engine, '1.0')
+// })
 
-test('spg test', () => {
+test('spg test', async () => {
   // arrange
-
+  // console.log(app)
   // assert
+  console.log('here')
   expect(1).toBe(1)
+  // await request(app).get('/info').expect(200)
 })
