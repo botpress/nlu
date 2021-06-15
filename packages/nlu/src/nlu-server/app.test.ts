1c7b908ee8539a7bfc86467239b056cc444cdce6
Simon-Pierre Gingras <892367+spg@users.noreply.github.com>
2021-06-15 19:02:19 -0400

    Don't validate body

--- packages/nlu/src/nlu-server/app.test.ts
+++ packages/nlu/src/nlu-server/app.test.ts
@@ -41,39 +41,23 @@ afterEach(() => {
   watcher.close()
 })
 
-describe('Without JWKS', () => {
-  test('GET /unknown-path', async () => {
-    const engine = await makeEngine(options, launcherLogger)
-    const app = await createApp(options, engine, version, watcher)
+test('GET /unknown-path', async () => {
+  const engine = await makeEngine(options, launcherLogger)
+  const app = await createApp(options, engine, version, watcher)
 
-    await request(app).get('/unknown-path').expect(404)
-  })
+  await request(app).get('/unknown-path').expect(404)
+})
 
-  test.each(['/info', '/v1/info'])('GET %s', async (path) => {
-    const engine = await makeEngine(options, launcherLogger)
-    const app = await createApp(options, engine, version, watcher)
+test.each(['/info', '/v1/info'])('GET %s', async (path) => {
+  const engine = await makeEngine(options, launcherLogger)
+  const app = await createApp(options, engine, version, watcher)
 
-    await request(app)
-      .get(path)
-      .expect(200, {
-        success: true,
-        info: {
-          health: {
-            isEnabled: true,
-            validProvidersCount: 1,
-            validLanguages: ['ar', 'de', 'en', 'es', 'fr', 'he', 'it', 'ja', 'nl', 'pl', 'pt', 'ru']
-          },
-          specs: { nluVersion: '2.2.0', languageServer: { dimensions: 300, domain: 'bp', version: '1.0.0' } },
-          languages: ['ar', 'de', 'en', 'es', 'fr', 'he', 'it', 'ja', 'nl', 'pl', 'pt', 'ru'],
-          version: '2.24.0'
-        }
-      })
-  })
+  await request(app).get(path).expect(200)
+})
 
-  test('GET /models', async () => {
-    const engine = await makeEngine(options, launcherLogger)
-    const app = await createApp(options, engine, version, watcher)
+test('GET /models', async () => {
+  const engine = await makeEngine(options, launcherLogger)
+  const app = await createApp(options, engine, version, watcher)
 
-    await request(app).get('/models').expect(200, { success: true, models: [] })
-  })
+  await request(app).get('/models').expect(200, { success: true, models: [] })
 })
