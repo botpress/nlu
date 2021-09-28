456230450f4ca7f42ebb7f6d5841545db22dbc88
François Levasseur <francois_levasseur@hotmail.com>
2021-09-28 15:22:58 -0400

    feat(nlu-client): make nlu-client configurable with an axios config (#104)
    * feat(nlu-client): make nlu-client configurable with an axios config
    
    * update joi types to 17.2.3
    
    * allow extra keys to be of type any

---
+++ packages/nlu-client/src/validation.ts
@@ -0,0 +1,38 @@
+import Joi from 'joi'
+import { SuccessReponse, ErrorResponse } from './typings/http'
+
+const allowUnknownKeys = (obj: Joi.ObjectSchema): Joi.ObjectSchema => {
+  return obj.pattern(/./, Joi.any())
+}
+
+const ERROR_RESPONSE_SCHEMA = allowUnknownKeys(
+  Joi.object().keys({
+    success: Joi.boolean().strict().not(true).required(),
+    error: Joi.string().allow('').required()
+  })
+)
+
+const SUCCESS_RESPONSE_SCHEMA = allowUnknownKeys(
+  Joi.object().keys({
+    success: Joi.boolean().strict().not(false).required()
+  })
+)
+
+const RESPONSE_SCHEMA = Joi.object()
+  .keys({ success: Joi.boolean().required() })
+  .when('.success', {
+    switch: [
+      {
+        is: true,
+        then: SUCCESS_RESPONSE_SCHEMA
+      },
+      {
+        is: false,
+        then: ERROR_RESPONSE_SCHEMA
+      }
+    ]
+  })
+
+export const validateResponse = <S extends SuccessReponse>(res: any): S | ErrorResponse => {
+  return Joi.attempt(res, RESPONSE_SCHEMA)
+}
