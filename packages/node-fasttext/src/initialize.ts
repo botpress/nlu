e22b7796f8589883f9bf11519c8b03ad786ad445
franklevasseur <francois_levasseur@hotmail.com>
2021-11-23 18:38:52 -0500

    chore(node-bindings): standard behavior, added env for debugging and split env var by pkg

---
+++ packages/node-fasttext/src/initialize.ts
@@ -0,0 +1,93 @@
+import fs from 'fs'
+import getos from 'getos'
+import path from 'path'
+import yn from 'yn'
+
+/* ###################
+ * ### UPDATE HERE ###
+ * ################### */
+const binName = 'fasttext'
+
+const fileName = `${binName}.node`
+const packageName = `node-${binName}`
+const customLocationEnv = `NODE_${binName.toUpperCase()}_DIR`
+const verboseEnv = `NODE_${binName.toUpperCase()}_VERBOSE`
+
+const defaultNativeExtensionsPath = path.join(__dirname, '..', 'native-extensions')
+const envNativeExtensionsPath =
+  process.env?.[customLocationEnv] && fs.existsSync(process.env[customLocationEnv]!) && process.env[customLocationEnv]
+
+const nativeExtensionsPath = envNativeExtensionsPath || defaultNativeExtensionsPath
+
+const verbose: boolean = !!yn(process.env[verboseEnv])
+const debuglog = (...msg: any[]): void => {
+  if (!verbose) {
+    return
+  }
+  const prefix = `[${packageName.toUpperCase()}]`
+  // eslint-disable-next-line no-console
+  console.log(prefix, ...msg)
+}
+
+const requireExtension = (os: string, distribution: string) => {
+  const filePath = path.join(nativeExtensionsPath, os, distribution, fileName)
+  debuglog(`about to require file "${filePath}"`)
+  return require(filePath)
+}
+
+const getOS = async (): Promise<getos.Os> => {
+  return new Promise((resolve, reject) => {
+    getos((err, distro) => {
+      if (err) {
+        reject(err)
+      }
+      resolve(distro)
+    })
+  })
+}
+
+export const init = async <T>(): Promise<T> => {
+  debuglog('initializing...')
+
+  const distro = await getOS()
+
+  debuglog('operating system: ', distro)
+
+  if (distro.os === 'win32') {
+    const binding = requireExtension('windows', 'all')
+    debuglog('success')
+    return binding
+  }
+  if (distro.os === 'darwin') {
+    const binding = requireExtension('darwin', 'all')
+    debuglog('success')
+    return binding
+  }
+
+  if (distro.os === 'linux') {
+    const distribution = distro.dist.toLowerCase()
+
+    debuglog('linux distribution: ', distro)
+
+    const relevantFolders = fs
+      .readdirSync(path.join(nativeExtensionsPath, 'linux'))
+      .filter((dir) => dir.startsWith(distribution))
+      .sort()
+      .reverse()
+    relevantFolders.push('default')
+
+    for (const dir of relevantFolders) {
+      try {
+        const binding = requireExtension('linux', dir) // this might throw
+        debuglog('success')
+        return binding
+      } catch (err) {
+        debuglog('error occured: ', err)
+      }
+    }
+
+    throw new Error(`Linux distribution ${distribution} is not supported by ${packageName}.`)
+  }
+
+  throw new Error(`The plateform ${distro.os} is not supported by ${packageName}.`)
+}
--- packages/node-svm/src/index.ts
+++ packages/node-svm/src/index.ts
@@ -1,6 +1,4 @@
-import fs from 'fs'
-import getos from 'getos'
-import path from 'path'
+import { init } from './initialize'
 import { NSVM } from './typings'
 
 type SvmCtor = new (args?: { random_seed: number }) => NSVM
@@ -10,70 +8,11 @@ interface BindingType {
   hello: HelloWorld
 }
 
-const defaultNativeExtensionsPath = path.join(__dirname, '..', 'native-extensions')
-const envNativeExtensionsPath =
-  process.env?.NATIVE_EXTENSIONS_DIR &&
-  fs.existsSync(process.env.NATIVE_EXTENSIONS_DIR) &&
-  process.env.NATIVE_EXTENSIONS_DIR
-
-const nativeExtensionsPath = envNativeExtensionsPath || defaultNativeExtensionsPath
-const fileName = 'node-svm.node'
-const packageName = 'node-svm'
-
-function requireExtension(os: string, distribution: string) {
-  const filePath = path.join(nativeExtensionsPath, os, distribution, fileName)
-  return require(filePath)
-}
-
-async function getOS(): Promise<getos.Os> {
-  return new Promise((resolve, reject) => {
-    getos((err, distro) => {
-      if (err) {
-        reject(err)
-      }
-      resolve(distro)
-    })
-  })
-}
-
-const init = async (): Promise<BindingType> => {
-  const distro = await getOS()
-
-  if (distro.os === 'win32') {
-    return requireExtension('windows', 'all')
-  }
-  if (distro.os === 'darwin') {
-    return requireExtension('darwin', 'all')
-  }
-
-  if (distro.os === 'linux') {
-    const distribution = distro.dist.toLowerCase()
-
-    const relevantFolders = fs
-      .readdirSync(path.join(nativeExtensionsPath, 'linux'))
-      .filter((dir) => dir.startsWith(distribution))
-      .sort()
-      .reverse()
-    relevantFolders.push('default')
-
-    for (const dir of relevantFolders) {
-      try {
-        const nsvm = requireExtension('linux', dir) // this might throw
-        return nsvm
-      } catch (err) {}
-    }
-
-    throw new Error(`Linux distribution ${distribution} is not supported by ${packageName}.`)
-  }
-
-  throw new Error(`The plateform ${distro.os} is not supported by ${packageName}.`)
-}
-
 let binding: BindingType | undefined
 export const makeSvm = async (args?: { random_seed: number }) => {
   if (binding) {
     return new binding.NSVM(args)
   }
-  binding = await init()
+  binding = await init<BindingType>()
   return new binding.NSVM(args)
 }
