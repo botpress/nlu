e851d85f4ee57953e83015ec8e48c1ce33cb20b0
mike <10071388+michaelmass@users.noreply.github.com>
2021-11-24 08:13:26 -0500

    chore(codestyle): updated codestyle using prettier

--- packages/nlu-server/src/telemetry/metric/meter.ts
+++ packages/nlu-server/src/telemetry/metric/meter.ts
@@ -4,10 +4,10 @@ export const trainingDuration = new client.Histogram({
   name: 'training_duration_seconds',
   help: 'Histogram of training duration in seconds.',
   labelNames: ['status'],
-  buckets: [0.1, 0.5, 1, 5, 15, 30, 60],
+  buckets: [0.1, 0.5, 1, 5, 15, 30, 60]
 })
 
 export const trainingCount = new client.Gauge({
   name: 'training_count',
-  help: 'Gauge of all trainings.',
+  help: 'Gauge of all trainings.'
 })
