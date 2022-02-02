e810d2e298d6f7433f4044c01c4db23d282dc1a7
franklevasseur <francois_levasseur@hotmail.com>
2022-02-02 11:26:46 -0500

    add more error info

--- packages/nlu-server/src/api/validation/app-id.ts
+++ packages/nlu-server/src/api/validation/app-id.ts
@@ -4,7 +4,7 @@ const APP_ID_PATTERN = /^[a-zA-Z_][a-zA-Z_\-\/0-9]*$/
 
 export const validateAppId = (appId: string): string => {
   if (!APP_ID_PATTERN.test(appId)) {
-    throw new InvalidRequestFormatError('App Id contains invalid characters')
+    throw new InvalidRequestFormatError(`App Id contains invalid characters: ${appId}`)
   }
   return appId
 }
