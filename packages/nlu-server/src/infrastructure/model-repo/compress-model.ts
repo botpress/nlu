a253a1ac7a62dddf303451f57cde507d90f8ff98
franklevasseur <francois_levasseur@hotmail.com>
2022-02-10 10:23:00 -0500

    chore(nlu-server): no more need to compress model

--- packages/nlu-server/src/infrastructure/model-repo/compress-model.ts
+++ packages/nlu-server/src/infrastructure/model-repo/compress-model.ts
@@ -1,11 +1,6 @@
 import { Model } from '@botpress/nlu-engine'
 import * as ptb from '@botpress/ptb-schema'
-import fse, { WriteStream } from 'fs-extra'
 import _ from 'lodash'
-import path from 'path'
-import { Stream } from 'stream'
-import tar from 'tar'
-import tmp from 'tmp'
 
 const PTBModelId = new ptb.PTBMessage('ModelId', {
   specificationHash: { type: 'string', id: 1 },
@@ -29,43 +24,10 @@ export const compressModel = async (model: Model): Promise<Buffer> => {
     finishedAt: finishedAt.toISOString(),
     data
   })
-
-  // TODO replace that logic with in-memory streams
-  const tmpDir = tmp.dirSync({ unsafeCleanup: true })
-  const tmpFileName = 'model'
-  const tmpFilePath = path.join(tmpDir.name, tmpFileName)
-  await fse.writeFile(tmpFilePath, Buffer.from(serialized))
-  const archiveName = 'archive'
-  const archivePath = path.join(tmpDir.name, archiveName)
-  await tar.create(
-    {
-      file: archivePath,
-      cwd: tmpDir.name,
-      portable: true,
-      gzip: true
-    },
-    [tmpFileName]
-  )
-  const buffer = await fse.readFile(archivePath)
-  tmpDir.removeCallback()
-  return buffer
+  return Buffer.from(serialized)
 }
 
 export const decompressModel = async (buffer: Buffer): Promise<Model> => {
-  const buffStream = new Stream.PassThrough()
-  buffStream.end(buffer)
-  const tmpDir = tmp.dirSync({ unsafeCleanup: true })
-
-  const tarFileName = 'model'
-  const tarStream = tar.x({ cwd: tmpDir.name, strict: true }, [tarFileName]) as WriteStream
-  buffStream.pipe(tarStream)
-  await new Promise((resolve) => tarStream.on('close', resolve))
-
-  const modelBuff = await fse.readFile(path.join(tmpDir.name, tarFileName))
-  try {
-    const { id, finishedAt, startedAt, data } = PTBModel.decode(modelBuff)
-    return { id, finishedAt: new Date(finishedAt), startedAt: new Date(startedAt), data: Buffer.from(data) }
-  } finally {
-    tmpDir.removeCallback()
-  }
+  const { id, finishedAt, startedAt, data } = PTBModel.decode(buffer)
+  return { id, finishedAt: new Date(finishedAt), startedAt: new Date(startedAt), data: Buffer.from(data) }
 }
