dc42ba4bf1662be2188d9436a71e9d4a429eae18
franklevasseur <francois_levasseur@hotmail.com>
2022-03-01 14:03:42 -0500

    fix(engine): e_000 is not affected by leading or consecutive spaces

--- packages/nlu-engine/src/engine/linting/e_000.ts
+++ packages/nlu-engine/src/engine/linting/e_000.ts
@@ -170,9 +170,10 @@ const flattenDataset = async (
     utterances.map((u, i) => ({ rawUtterance: u, intent: x, utteranceIdx: i }))
   )
 
-  const rawUtterances: string[] = flatRawUtterances.map(({ rawUtterance }) => rawUtterance).map((u) => u.trim())
+  const rawUtterances: string[] = flatRawUtterances.map(({ rawUtterance }) => rawUtterance)
   const utteranceBatch = await buildUtteranceBatch(rawUtterances, ts.language, tools, [], {
-    vectorize: false // no need for vectors to go faster
+    vectorize: false, // no need for vectors to go faster
+    preprocess: false // all characters must be kept
   })
 
   const flatUtterances = _.zip(flatRawUtterances, utteranceBatch)
---
+++ packages/nlu-engine/src/engine/tools/tag-spaces.ts
@@ -0,0 +1,24 @@
+export type Span = {
+  charStart: number
+  charEnd: number
+  length: number
+}
+
+const getSpan = (regexpMatch: RegExpExecArray): Span => ({
+  charStart: regexpMatch.index,
+  charEnd: regexpMatch.index + regexpMatch[0].length,
+  length: regexpMatch[0].length
+})
+
+const SPACE_GROUP = /\s{1,}/g
+
+export const tagAllSpaces = (utt: string): Span[] => {
+  const spans: Span[] = []
+  let match = SPACE_GROUP.exec(utt)
+  while (match) {
+    const span = getSpan(match)
+    spans.push(span)
+    match = SPACE_GROUP.exec(utt)
+  }
+  return spans
+}
